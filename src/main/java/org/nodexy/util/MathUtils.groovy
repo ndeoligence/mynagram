f71f61559183b98fa25117835e94648e9b40fca0
Monde Ngalonkulu <monde.ngalonkulu@nimbletech.co.za>
2017-02-06 13:38:12 +0200

    Add assertions

--- src/main/java/org/nodexy/util/MathUtils.groovy
+++ src/main/java/org/nodexy/util/MathUtils.groovy
@@ -4,13 +4,15 @@ package org.nodexy.util
  * Created by phoenix on 2/4/17.
  */
 class MathUtils {
-    def static Number p(Number n, Number r) {
+    static Number p(Number n, Number r) {
+        assert n>=r
         factorial(n) / factorial(n-r)
     }
-    def static Number c(Number n, Number r) {
+     static Number c(Number n, Number r) {
+        assert n>=r
         factorial(n) / (factorial(n-r)*factorial(r))
     }
-    def static Number factorial(Number x) {
+     static Number factorial(Number x) {
         assert x >= 0
         BigInteger f = 1
         if (x > 1) 1.upto x, {f *= it}
